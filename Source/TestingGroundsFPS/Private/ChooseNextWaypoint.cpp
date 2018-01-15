2dd747974798f05d1cb6a6f8e6966045925a0052
DESKTOP-R3DN5G5\rocke <ktnp1204@gmail.com>
2018-01-15 15:28:31 +0530

    AI characters move to waypoints using C++ instead of BP

--- Source/TestingGroundsFPS/Private/ChooseNextWaypoint.cpp
+++ Source/TestingGroundsFPS/Private/ChooseNextWaypoint.cpp
@@ -2,11 +2,30 @@
 
 #include "Public/ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "Classes/AIController.h"
+#include "Public/PatrollingGuard.h"		// TODO Remove coupling
+
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	// TODO protect against empty patrol routes
+
+	// Get the patrol points
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+
+	// Set next waypoint
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
-	UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %i"),Index)
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	// Cycle the index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+
+	//UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %i"),Index)
+	
 	return EBTNodeResult::Succeeded;
 }
